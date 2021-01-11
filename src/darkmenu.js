7e9e5c37d2eacb02a224d15e518ac45f7e19b658
ninem005 <76542279+ninem005@users.noreply.github.com>
2021-01-10 21:03:04 -0400

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,40 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Ninem:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+
+  TRADUZIDO POR *NINEM*
+  DUVIDAS? 👇
+  WA.me/5569993733829`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
+
---
+++ src/help1.js
@@ -0,0 +1,40 @@
+const help1 = (prefix) => {
+
+	return `
+
+ *COMANDOS DO NINEM*
+
+ 
+➸ *${prefix}marcar*
+➸ *${prefix}marcar2*
+➸ *${prefix}marcar3*
+➸ *${prefix}loli*
+➸ *${prefix}loli1*
+➸ *${prefix}hentai*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}akeno*
+➸ *${prefix}meme*
+➸ *${prefix}lofi*
+➸ *${prefix}malkova*
+➸ *${prefix}nsfwloli*
+➸ *${prefix}reislin*
+➸ *${prefix}limpar*
+➸ *${prefix}marcar*
+➸ *${prefix}ts (texto que deseja transmitir)*
+
+fim😌❤️
+
+}
+exports.help1 = help1
+
+
